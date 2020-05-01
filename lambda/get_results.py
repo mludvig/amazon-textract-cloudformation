58c47f53f201e2c363fa90d2336c88ff48cee773
Michael Ludvig <mludvig@logix.net.nz>
2020-05-01 14:12:06 +1200

    Compile xyz.pdf.blocks.json file for convenience

--- lambda/get_results.py
+++ lambda/get_results.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Retrieve Textract results and store them to S3
+
 import os
 import json
 import time
@@ -7,19 +9,6 @@ import boto3
 
 s3_client = boto3.client('s3')
 textract_client = boto3.client('textract')
-
-def startJob(bucket_name, object_name):
-    response = textract_client.start_document_text_detection(
-        DocumentLocation={
-            'S3Object': {
-                'Bucket': bucket_name,
-                'Name': object_name,
-            }
-        }
-    )
-    print(f"JobID: {response['JobId']}")
-
-    return response["JobId"]
 
 def getJobResults(job_id, next_token = None):
     kwargs = {}
@@ -54,9 +43,11 @@ def lambda_handler(event, context):
 
     output_object_base = os.path.join(output_prefix, os.path.basename(input_object))
 
-    event['raw_bucket'] = output_bucket
-    event['raw_objects'] = []
+    event['output_bucket'] = output_bucket
+    event['raw_results'] = []
     page_counter = 0
+    blocks = []
+
     while True:
         page_counter += 1
         output_object = f"{output_object_base}.raw.{page_counter:02d}.json"
@@ -67,8 +58,11 @@ def lambda_handler(event, context):
             ServerSideEncryption='AES256',
             ContentType='application/json',
         )
-        event['raw_objects'].append(output_object)
-        print(f"Page {page_counter:02} saved to: s3://{output_bucket}/{output_object}")
+        event['raw_results'].append(output_object)
+        print(f"Result {page_counter:02} saved to: s3://{output_bucket}/{output_object}")
+
+        if 'Blocks' in results:
+            blocks.extend(results['Blocks'])
 
         if 'NextToken' not in results:
             break
@@ -76,14 +70,16 @@ def lambda_handler(event, context):
         print(f"NextToken: {results['NextToken']}")
         results = getJobResults(job_id, next_token=results['NextToken'])
 
-    return event
+    # Save merged 'Blocks' into a separate file for ease of use
+    output_object = f"{output_object_base}.blocks.json"
+    s3_client.put_object(
+        Bucket=output_bucket,
+        Key=output_object,
+        Body=json.dumps(blocks),
+        ServerSideEncryption='AES256',
+        ContentType='application/json',
+    )
+    print(f"Blocks saved to: s3://{output_bucket}/{output_object}")
+    event['blocks'] = output_object
 
-if __name__ == "__main__":
-    event = {
-        "bucket_name": "s3-step-bucket-1tjhxv88nuo0p",
-        "object_name": "upload/template-1588283708.yml",
-        "job_id": "54625eb1fb8e44c3f3877e704e6ea4483497890c6ff4cb5e7435250b77f921bf",
-        "job_start_timestamp": 1588283713.1570575
-    }
-    ret = lambda_handler(event, {})
-    print("lambda_handler(): ", json.dumps(ret))
+    return event
